dc56795cd836ff99dbf4c8f1306d234ee01abfba
daniel-ewing <daniel.ewing@camunda.com>
2022-03-14 12:43:07 -0400

    Test for input mapping after an event based gateway.

--- src/main/java/com/dewing/spring/boot/test1/delegate/SendMessage1.java
+++ src/main/java/com/dewing/spring/boot/test1/delegate/SendMessage1.java
@@ -1,20 +1,33 @@
 package com.dewing.spring.boot.test1.delegate;
 
 import lombok.extern.slf4j.Slf4j;
+import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Component
 @Slf4j
-public class DelegateTask implements JavaDelegate {
+public class SendMessage1 implements JavaDelegate {
+    private final String messageName = "message-1";
+    private RuntimeService runtimeService;
+
+    public SendMessage1(RuntimeService runtimeService) {
+        this.runtimeService = runtimeService;
+    }
 
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
         if (log.isDebugEnabled()) log.debug("-----> execute: Enter - {}", delegateExecution.getCurrentActivityId());
 
-        //TODO:
-        //Do something.
+        Map<String, Object> vars = new HashMap<>();
+        vars.put("variable1", "value1");
+        vars.put("fileId", "other/stuff");
+
+        runtimeService.correlateMessage(messageName, delegateExecution.getProcessBusinessKey(), vars);
 
         if (log.isDebugEnabled()) log.debug("-----> execute: Exit - {}", delegateExecution.getCurrentActivityId());
     }
--- src/main/java/com/dewing/spring/boot/test1/delegate/SendMessage2.java
+++ src/main/java/com/dewing/spring/boot/test1/delegate/SendMessage2.java
@@ -1,22 +1,33 @@
 package com.dewing.spring.boot.test1.delegate;
 
 import lombok.extern.slf4j.Slf4j;
+import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Component
 @Slf4j
-public class GetVariables implements JavaDelegate {
+public class SendMessage2 implements JavaDelegate {
+    private final String messageName = "message-2";
+    private RuntimeService runtimeService;
+
+    public SendMessage2(RuntimeService runtimeService) {
+        this.runtimeService = runtimeService;
+    }
 
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
         if (log.isDebugEnabled()) log.debug("-----> execute: Enter - {}", delegateExecution.getCurrentActivityId());
 
-        Map<String, Object> vars = delegateExecution.getVariables();
-        vars.forEach( (key, var) -> { if (log.isDebugEnabled()) log.debug("-----> {} = {}", key, var); });
+        Map<String, Object> vars = new HashMap<>();
+        vars.put("variable1", "value1");
+        vars.put("fileId", "other/stuff");
+
+        runtimeService.correlateMessage(messageName, delegateExecution.getProcessBusinessKey(), vars);
 
         if (log.isDebugEnabled()) log.debug("-----> execute: Exit - {}", delegateExecution.getCurrentActivityId());
     }
