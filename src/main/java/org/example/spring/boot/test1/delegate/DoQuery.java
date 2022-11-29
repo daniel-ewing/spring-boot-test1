cf58ae0ac5c8f7d7fa82a7322c7f6c464b607db9
daniel-ewing <daniel.ewing@camunda.com>
2022-11-29 12:36:04 -0500

    Test for TaskQuery#taskCandidateGroup.

--- src/main/java/org/example/spring/boot/test1/delegate/DoQuery.java
+++ src/main/java/org/example/spring/boot/test1/delegate/DoQuery.java
@@ -1,21 +1,29 @@
 package org.example.spring.boot.test1.delegate;
 
 import lombok.extern.slf4j.Slf4j;
+import org.camunda.bpm.engine.TaskService;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.springframework.stereotype.Component;
 
-@Component("DelegateTask")
+import java.util.List;
+
+@Component("DoQuery")
 @Slf4j
-public class DelegateTask implements JavaDelegate {
+public class DoQuery implements JavaDelegate {
+    private TaskService taskService;
+
+    DoQuery(TaskService taskService) {
+        this.taskService = taskService;
+    }
 
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
         if (log.isDebugEnabled()) log.debug("-----> execute: Enter - {}", delegateExecution.getCurrentActivityId());
 
-        //TODO:
-        //Do something.
+        List taskList = taskService.createTaskQuery().taskCandidateGroup("test.group").list();
 
+        if (log.isDebugEnabled()) log.debug("-----> execute: taskList.size() - {}", taskList.size());
         if (log.isDebugEnabled()) log.debug("-----> execute: Exit - {}", delegateExecution.getCurrentActivityId());
     }
 }
