c9ada5e2f71b2c90f39b2cb10d94da4254fb5244
daniel-ewing <daniel.ewing@camunda.com>
2024-07-10 16:08:33 -0400

    Test case for SUPPORT-22688.

--- src/main/java/org/example/spring/boot/test1/ProcessApplication.java
+++ src/main/java/org/example/spring/boot/test1/ProcessApplication.java
@@ -8,12 +8,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.event.EventListener;
 
+import java.util.*;
+
 @SpringBootApplication
 @EnableProcessApplication
 @Slf4j
 public class ProcessApplication {
-	private final static String processKey = "process";
-	private RuntimeService runtimeService;
+	private final static String processKey = "test";
+	private final RuntimeService runtimeService;
 
 	public ProcessApplication(RuntimeService runtimeService) {
 		this.runtimeService = runtimeService;
@@ -23,8 +25,13 @@ public class ProcessApplication {
 	private void processPostDeploy(PostDeployEvent event) {
 		if (log.isDebugEnabled()) log.debug("-----> processPostDeploy: Enter");
 
+//		List<String> ids = new ArrayList<>(Arrays.asList("1", "2")); // use this if you want to see synchronous modification work.
+		List<String> ids = new ArrayList<>(Arrays.asList("1"));
+		Map<String, Object> vars = new HashMap<>();
+		vars.put("ids", ids);
+
 		for (int pi = 1; pi <= 1; pi++) {
-			runtimeService.startProcessInstanceByKey(processKey, processKey + " bk " + pi);
+			runtimeService.startProcessInstanceByKey(processKey, processKey + " bk " + pi, vars);
 			if ((pi % 1000) == 0) {
 				if (log.isDebugEnabled()) log.debug("-----> processPostDeploy created: {} process instances", pi);
 			}
